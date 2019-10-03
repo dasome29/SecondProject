4c5fd01a83e576b09c914cef48884a7765027b98
David Solís <david.solis.mendez@gmail.com>
2019-10-03 06:49:05 -0600

    Binary tree and main class.

---
+++ src/BinaryTree/BinaryTree.java
@@ -0,0 +1,66 @@
+package BinaryTree;
+
+public class BinaryTree{
+    private Node root = null;
+
+
+    boolean isEmpty(){
+        return root == null;
+    }
+
+
+    boolean contains(String element){
+        return this.contains(element, root);
+    }
+
+    private boolean contains(String element, Node current){
+        if(current == null){
+            return false;
+        }
+        else if(element.compareTo(current.element) < 0){
+            return contains(element, current.left);
+        }
+        else if(element.compareTo(current.element) > 0){
+            return contains(element, current.right);
+        }
+        else{
+            return true;
+        }
+    }
+
+
+    public void insert(String element){
+        root = this.insert(element, root);
+    }
+
+
+    private Node insert(String element, Node current){
+        if(current == null){
+            return new Node(element);
+        }
+        else if(element.compareTo(current.element) < 0){
+            current.left = insert(element, current.left);
+        }
+        else if(element.compareTo(current.element) > 0){
+            current.right = insert(element, current.right);
+        }else{
+            return current;
+        }
+        return current;
+    }
+}
+
+
+
+
+class Node {
+    String element;
+    Node left;
+    Node right;
+
+
+    public Node(String element){
+        this.element = element;
+    }
+}
+
