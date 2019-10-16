6d5b1f54b132fa3ce7d3dec52e70965f092ebed3
Sebastian Mora <semasmora@gmail.com>
2019-10-16 03:05:35 -0600

    Cambios menores

--- src/BinaryTree/Node.java
+++ src/BinaryTree/Node.java
@@ -11,19 +11,56 @@ import java.io.File;
  */
 
 public class Node {
-    String element;
-    private LinkedArrayList<File> recurrences = new LinkedArrayList<File>();
-    Node left;
-    Node right;
+    public String element;
+    public LinkedArrayList<File> recurrences = new LinkedArrayList<File>();
+    //private LinkedArrayList<LinkedArrayList<LinkedArrayList>> recurrences = new LinkedArrayList<LinkedArrayList<LinkedArrayList>>();
+
+    public Node left;
+    public Node right;
 
 
     /**
      *  Constructor del nodo
      * @param element
      */
-    Node(String element){
+    public Node(String element){
         this.element = element;
     }
+
+
+
+    /*
+    public void setDocument(File file, LinkedArrayList<int[]> mat){
+        LinkedArrayList<File> document = new LinkedArrayList<File>();
+        document.addLast(file);
+
+        LinkedArrayList<LinkedArrayList> pairs = new LinkedArrayList<LinkedArrayList>();
+        pairs.addLast(document);
+        pairs.addLast(mat);
+
+        recurrences.addLast(pairs);
+    }
+    public LinkedArrayList<File> getDocuments(){
+        LinkedArrayList<File> docs = new LinkedArrayList<File>();
+        for (int i = 0; i < recurrences.getSize(); i++) {
+            docs.addLast((File) recurrences.getElement(i).getElement(0).getElement(0));
+        }
+        return docs;
+    }
+    public LinkedArrayList getPositions(File file){
+        LinkedArrayList<int[]> result = new LinkedArrayList<int[]>();
+        for (int i = 0; i < recurrences.getSize(); i++) {
+            if (file.equals(recurrences.getElement(i).getElement(0).getElement(0))){
+                for (int j = 0; j<recurrences.getElement(i).getElement(1).getSize();j++){
+                    result.addLast((int[]) recurrences.getElement(i).getElement(1).getElement(j));
+                }
+            }
+        }
+        return result;
+    }
+
+     */
+
 
     /**
      * Método que retorn la lista  de concurrencias de la palanbra
